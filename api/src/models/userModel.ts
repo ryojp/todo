d05893398713f7afcfe4fe6b927bc875d68c10d9
Ryo Koike <ryok022366@gmail.com>
2022-10-14 23:05:08 +0900

    api-db: impl user model and its tests

---
+++ api/src/models/userModel.ts
@@ -0,0 +1,134 @@
+import { Model, Schema, model, Document } from "mongoose";
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
+export const MAX_LOGIN_ATTEMPTS = 5;
+export const LOGIN_LOCK_TIME_MS = 2 * 60 * 60 * 1000; // 2 hr
+
+export interface IUser {
+  username: string;
+  password: string; // hashed on save
+  loginAttempts: number;
+  lockUntil?: number;
+}
+
+export interface IUserDoc extends Document, IUser {
+  comparePassword(candidatePassword: string): Promise<boolean>;
+  incLoginAttempts(): Promise<void>;
+  isLocked(): boolean;
+}
+
+export enum LoginResult {
+  SUCCESS,
+  NOT_FOUND,
+  PASSWORD_INCORRECT,
+  MAX_ATTEMPTS,
+}
+
+interface UserModel extends Model<IUserDoc> {
+  findByUsername(username: string): Promise<IUserDoc | null>;
+  getAuthenticated(username: string, password: string): Promise<LoginResult>;
+}
+
+const UserSchema = new Schema<IUserDoc, UserModel>({
+  username: {
+    type: String,
+    required: true,
+    index: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  loginAttempts: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  lockUntil: Number,
+});
+
+UserSchema.method<IUserDoc>(
+  "comparePassword",
+  async function (candidate_password: string) {
+    const match = await bcrypt.compare(candidate_password, this.password);
+    return match;
+  }
+);
+
+UserSchema.method<IUserDoc>(
+  "incLoginAttempts",
+  async function (): Promise<void> {
+    this.loginAttempts++;
+
+    // lock the account if we've reached max attempts and it's not locked already
+    if (this.loginAttempts >= MAX_LOGIN_ATTEMPTS && !this.isLocked()) {
+      this.lockUntil = Date.now() + LOGIN_LOCK_TIME_MS;
+    }
+
+    await this.save();
+  }
+);
+
+UserSchema.method<IUserDoc>("isLocked", function (): boolean {
+  return this.lockUntil! >= Date.now(); // `!` post-fix exp operator
+});
+
+UserSchema.static(
+  "findByUsername",
+  async function (username: string): Promise<IUserDoc | null> {
+    return this.findOne({ username });
+  }
+);
+
+UserSchema.static(
+  "getAuthenticated",
+  async function (username: string, password: string): Promise<LoginResult> {
+    const user = await this.findByUsername(username);
+
+    // make sure the user exists
+    if (!user) return LoginResult.NOT_FOUND;
+
+    // check if the account is currently locked
+    if (user.isLocked()) {
+      // just increment login attempts if account is already locked
+      await user.incLoginAttempts();
+      return LoginResult.MAX_ATTEMPTS;
+    }
+
+    // test for a matching password
+    const matched = await user.comparePassword(password);
+
+    if (!matched) {
+      // password is incorrect, so increment login attempts before responding
+      await user.incLoginAttempts();
+      return LoginResult.PASSWORD_INCORRECT;
+    }
+
+    // if there's no lock or failed attempts, return SUCCESS
+    if (!user.loginAttempts && !user.lockUntil) return LoginResult.SUCCESS;
+
+    // reset attempts and lock info
+    user.loginAttempts = 0;
+    delete user.lockUntil;
+
+    await user.save();
+
+    return LoginResult.SUCCESS;
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  // A new password has been set; let's hash it
+  const hashed = await bcrypt.hash(this.password, SALT_ROUNDS);
+  this.password = hashed;
+
+  next();
+});
+
+const User = model<IUserDoc, UserModel>("Users", UserSchema);
+
+export default User;
---
+++ api/test/user.test.ts
@@ -0,0 +1,120 @@
+import mongoose from "mongoose";
+import User, {
+  LOGIN_LOCK_TIME_MS,
+  LoginResult,
+  MAX_LOGIN_ATTEMPTS,
+} from "../src/models/userModel";
+
+describe("Test User DB", () => {
+  mongoose.Promise = global.Promise;
+
+  // connect to MongoDB before running each test case
+  beforeEach(async () => {
+    await mongoose
+      .connect("mongodb://mongo-test/", {
+        user: "testuser",
+        pass: "testpass",
+        dbName: "TestDB",
+      })
+      .catch((err) => {
+        console.log("Error connecting to the database\n" + err);
+      });
+  });
+
+  // drop MongoDB and close connection after running each test case
+  afterEach((done) => {
+    User.deleteMany({}, async () => {
+      await mongoose.disconnect();
+      done();
+    });
+  });
+
+  it("Authentication just after creation", async () => {
+    const username = "john";
+    const password = "abc";
+    await User.create({ username, password });
+
+    let result: LoginResult;
+
+    // ground truth
+    result = await User.getAuthenticated(username, password);
+    expect(result).toBe(LoginResult.SUCCESS);
+
+    // incorrect username
+    result = await User.getAuthenticated(username + " lobb", password);
+    expect(result).toBe(LoginResult.NOT_FOUND);
+
+    // incorrect password
+    result = await User.getAuthenticated(username, password + "def");
+    expect(result).toBe(LoginResult.PASSWORD_INCORRECT);
+  });
+
+  it("Unlock from too many trials", async () => {
+    const username = "john";
+    const password = "abc";
+    await User.create({ username, password });
+
+    let result: LoginResult;
+
+    // repeat trying to login with incorrect passwords
+    for (let i = 0; i < MAX_LOGIN_ATTEMPTS; i++) {
+      result = await User.getAuthenticated(username, password + i);
+      expect(result).toBe(LoginResult.PASSWORD_INCORRECT);
+    }
+
+    // should be locked; correct password should not work now
+    result = await User.getAuthenticated(username, password);
+    expect(result).toBe(LoginResult.MAX_ATTEMPTS);
+
+    // update Date.now()
+    const current_time_ms = Date.now();
+    Date.now = jest.fn(() => current_time_ms + LOGIN_LOCK_TIME_MS);
+
+    // should NOT be locked now
+    result = await User.getAuthenticated(username, password);
+    expect(result).toBe(LoginResult.SUCCESS);
+  });
+
+  it("Too many failed logins", async () => {
+    const username = "john";
+    const password = "abc";
+    await User.create({ username, password });
+
+    let result: LoginResult;
+
+    // repeat trying to login with incorrect passwords
+    for (let i = 0; i < MAX_LOGIN_ATTEMPTS; i++) {
+      result = await User.getAuthenticated(username, password + i);
+      expect(result).toBe(LoginResult.PASSWORD_INCORRECT);
+    }
+
+    // should be locked; correct password should not work now
+    result = await User.getAuthenticated(username, password);
+    expect(result).toBe(LoginResult.MAX_ATTEMPTS);
+
+    // of course, an incorrect password is not accepted
+    result = await User.getAuthenticated(username, "incorrect pass");
+    expect(result).toBe(LoginResult.MAX_ATTEMPTS);
+  });
+
+  it("Authentication after updating password", async () => {
+    const username = "john";
+    const password = "abc";
+    const user = await User.create({ username, password });
+
+    // update password
+    const new_pass = "def";
+    user.password = new_pass;
+    await user.save();
+
+    let result: LoginResult;
+
+    // ground truth
+    result = await User.getAuthenticated(username, new_pass);
+    expect(result).toBe(LoginResult.SUCCESS);
+
+    // old password
+    result = await User.getAuthenticated(username, password);
+    expect(result).toBe(LoginResult.PASSWORD_INCORRECT);
+  });
+});
