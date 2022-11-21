5cf9036f7a5928ed065554fa30cd428d3a0b383d
Ryo Koike <ryok022366@gmail.com>
2022-11-21 18:23:56 +0900

    misc: setup CORS and remove nginx from dev compose
    Now, the frontend and the backend api can be hosted on different
    origins!

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from 'cors';
 
 import routes from "./routes/taskRoute";
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cors({
+  origin: [process.env['FRONTEND_URL'] as string],
+  methods: 'GET, POST, PUT, PATCH, DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 
 routes(app);
 
