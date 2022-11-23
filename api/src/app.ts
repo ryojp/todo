9304767490c09e859f3e9d46151e8cc26e853c06
Ryo Koike <ryok022366@gmail.com>
2022-11-23 16:23:31 +0900

    Revert "api: dummy commit"
    This reverts commit db48ddf916ebc5f6b58240c628850d0cd1c14ed4.

--- api/src/app.ts
+++ api/src/app.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: [process.env["FRONTEND_URL"] || "http://localhost:3000"],
+    origin: [process.env["FRONTEND_URL"] as string],
     methods: "GET, POST, PUT, PATCH, DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
   })
