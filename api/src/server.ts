e5ea98cc8192bd8b0144f9dbf5b6e5bd95a7a694
Ryo Koike <ryok022366@gmail.com>
2022-11-23 11:10:16 +0900

    api: set fallback default port

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import app from "./app";
 import { mongoURL, mongoUser, mongoPass, mongoDBName } from "./env";
 
-const port: number = +(process.env.PORT as string);
+const port = +(process.env.PORT || "4000");
 
 mongoose.Promise = global.Promise;
 mongoose
